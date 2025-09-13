74ef38994ff3cabed2b01c9aacafe7abf39755e6
Diego <diegodecstraid@gmail.com>
2025-09-13 02:23:44 -0300

    Exercicio 2 referente a estrutura estatica.

---
+++ Java1/src/ClassesAtributosMetodos/Employee.java
@@ -0,0 +1,36 @@
+package ClassesAtributosMetodos;
+
+public class Employee {
+    /*
+    Fazer um programa para ler os dados de um funcionário (nome, salário bruto e imposto). Em
+seguida, mostrar os dados do funcionário (nome e salário líquido). Em seguida, aumentar o
+salário do funcionário com base em uma porcentagem dada (somente o salário bruto é
+afetado pela porcentagem) e mostrar novamente os dados do funcionário. Use a classe
+projetada abaixo.
+
+Dados:
+Name: Joao Silva
+Gross salary: 6000.00
+Tax: 1000.00
+Employee: Joao Silva, $ 5000.00
+Which percentage to increase salary? 10.0
+Updated data: Joao Silva, $ 5600.00
+     */
+    public String name;
+    public double grossSalary;
+    public double tax;
+
+    public double netSalary() {
+        return grossSalary - tax;
+    }
+
+    public void increaseSalary(double percentage) {
+        grossSalary += grossSalary * percentage / 100.0;
+    }
+
+    public String toString() {
+        return name
+                + ", $ "
+                + String.format("%.2f", netSalary());
+    }
+}

15f6be89bf395ee8c34c5e798aee98eea7a0a377
Diego <diegodecstraid@gmail.com>
2026-02-18 00:19:00 -0300

    Resolution with exercise Headquarters

---
+++ Java1/src/MemoryBehaviorArraysLists/exercices/Headquarters.java
@@ -0,0 +1,38 @@
+package MemoryBehaviorArraysLists.exercices;
+
+import java.util.Scanner;
+
+public class Headquarters {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int [][] mat = new int[N][N];
+
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[i].length; j++) {
+                mat[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Main diagonal:");
+        for (int i = 0; i < mat.length; i++) {
+            System.out.println(mat[i][i] + "  ");
+        }
+        System.out.println();
+
+        int count = 0;
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[i].length; j++) {
+                if (mat[i][j] < 0) {
+                    count++;
+                }
+            }
+        }
+        System.out.println("Negative numbers:" + count);
+
+        sc.close();
+    }
+}

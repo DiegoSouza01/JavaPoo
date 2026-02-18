92de4626afb542ea4e0b25a113d2f7650f9b0013
Diego <diegodecstraid@gmail.com>
2026-02-17 23:57:08 -0300

    Refactor entities with Employee

--- Java1/src/MemoryBehaviorArraysLists/entities/Employee.java
+++ Java1/src/MemoryBehaviorArraysLists/entities/Employee.java
@@ -45,6 +45,7 @@ public class Employee {
         salary += salary * percentage / 100;
     }
 
+    @Override
     public String toString() {
         return id + ", " + name + ", " + String.format("%.2f", salary);
     }

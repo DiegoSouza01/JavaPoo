55b452f43290ad4c9ed6df8a368c26081131e732
Diego <diegodecstraid@gmail.com>
2026-02-17 23:56:19 -0300

    Exercise completed with Employee

---
+++ Java1/src/MemoryBehaviorArraysLists/entities/Employee.java
@@ -0,0 +1,51 @@
+package MemoryBehaviorArraysLists.entities;
+
+public class Employee {
+
+    private String name;
+    private Integer id;
+    private Double salary;
+
+    public Employee() {
+
+    }
+
+    public Employee(Double salary, String name, Integer id) {
+        super();
+        this.salary = salary;
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Double salary) {
+        this.salary = salary;
+    }
+
+    public void increaseSalary(double percentage) {
+        salary += salary * percentage / 100;
+    }
+
+    public String toString() {
+        return id + ", " + name + ", " + String.format("%.2f", salary);
+    }
+}

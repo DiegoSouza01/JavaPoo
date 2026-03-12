8a708cf2c861b1b17a274ee78da1ac04f453983f
Diego <diegodecstraid@gmail.com>
2026-03-11 23:29:10 -0300

    Resolution Exercise with polymorphism Company

---
+++ Java1/src/inheritanceAndPolymorphism/entities3/Company.java
@@ -0,0 +1,27 @@
+package inheritanceAndPolymorphism.entities3;
+
+public class Company extends TaxPayer {
+
+    private Integer numberOfEmployees;
+
+    public Company(){
+    }
+
+    public Company(String name, Double anualIncome, Integer numberOfEmployees) {
+        super(name, anualIncome);
+        this.numberOfEmployees = numberOfEmployees;
+    }
+
+    public Integer getNumberOfEmployees() {
+        return numberOfEmployees;
+    }
+
+    public void setNumberOfEmployees(Integer numberOfEmployees) {
+        this.numberOfEmployees = numberOfEmployees;
+    }
+
+    @Override
+    public double tax() {
+        return 0;
+    }
+}
